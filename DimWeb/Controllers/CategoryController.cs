cbfaded0aeddf54772b61790606b09cf9c528783
Dimitrios Porfiropoulos <dimiporf@live.com>
2023-11-14 20:55:02 +0100

    End of day 2

--- DimWeb/Controllers/CategoryController.cs
+++ DimWeb/Controllers/CategoryController.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DimWeb.Data;
+using DimWeb.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DimWeb.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;   
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
         }
     }
 }
