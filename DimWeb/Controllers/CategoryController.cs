20c057b2b9d62ed70d40ca9506a32f2210697690
Dimitrios Porfiropoulos <dimiporf@live.com>
2023-11-16 21:06:24 +0100

    End of day #3

--- DimWeb/Controllers/CategoryController.cs
+++ DimWeb/Controllers/CategoryController.cs
@@ -1,6 +1,7 @@
 ﻿using DimWeb.Data;
 using DimWeb.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics.Metrics;
 
 namespace DimWeb.Controllers
 {
@@ -15,6 +16,33 @@ namespace DimWeb.Controllers
         {
             List<Category> objCategoryList = _db.Categories.ToList();
             return View(objCategoryList);
+        }
+
+        public IActionResult Create() 
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString()) 
+            {
+                ModelState.AddModelError("name","The Display Order cannot exactly match the Category Name");
+            }
+
+            if (obj.Name!=null && obj.Name.ToLower() == "test")
+            {
+                ModelState.AddModelError("", "Test is one of the invalid values");
+            }
+
+            if (ModelState.IsValid)
+            { 
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+                return View(obj);
+
         }
     }
 }
